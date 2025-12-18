901e320c99fb6623f92709a625769b0399ec6890
CalderonRigoberto <jesuscalflo15@gmail.com>
2025-12-17 21:40:02 -0600

    fix: Update base path in Astro config for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://calderonrigoberto.github.io/presentacion-ejecutiva-fundacion',
+  base: '/presentacion-ejecutiva-fundacion/',
   compressHTML: true,
 });
 
