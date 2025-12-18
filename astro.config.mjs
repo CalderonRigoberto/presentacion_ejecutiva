a9991d55c9ccb7eb9fcdf127e8cf6f2cff65a574
CalderonRigoberto <jesuscalflo15@gmail.com>
2025-12-17 21:42:48 -0600

    chore: Update Astro config site and base paths, add output settings; enhance TypeScript paths for better module resolution

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://calderonrigoberto.github.io/presentacion-ejecutiva-fundacion',
-  base: '/presentacion-ejecutiva-fundacion/',
+  site: 'https://calderonrigoberto.github.io/presenteacion_ejecutiva',
+  base: '/presenteacion_ejecutiva/',
   compressHTML: true,
+  output: 'static',
+  vite: {
+    css: {
+      devSourcemap: true,
+    }
+  }
 });
 
